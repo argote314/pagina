ee302adf8b343b685f8a5ee1b94a146a776e6204
argote314 <76407732+argote314@users.noreply.github.com>
2021-03-14 18:28:57 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,23 @@
+var pala = 0
+const pagina = Math.floor(Math.random() * 4) + 1
+if(pagina==1){
+    const pal = '1';
+    pala = pal
+    document.write(pala);
+}
+if(pagina==2){
+    const pal = '2';
+    pala = pal
+    document.write(pala);
+}
+if(pagina==3){
+    const pal = '3';
+    pala = pal
+    document.write(pala);
+}
+else{
+    const pal = '4';
+    pala = pal
+    document.write(pala);
+}
+
